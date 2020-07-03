b1f9878fae2beb91f061a8a20ffb0c2414cd6076
Agnieszka Nowakowska <agnieszkanowakowska@imac-agnieszka.home>
2020-07-03 23:33:16 +0200

    Add descriptions to background photos

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-let img = ["zlodziej.jpg", "peoplebanner.jpg", "halfgirl.jpg", "cottoncanvas.jpg"];
+let mountainsPhoto = {
+    image: 'zlodziej.jpg',
+    description: 'Eons of Earth'
+};
+
+let peoplePhoto = {
+    image: 'peoplebanner.jpg',
+    description: 'Peaceful Stills'
+};
+
+let girlPhoto = {
+    image: 'halfgirl.jpg',
+    description: 'Group Work'
+};
+
+let cottonPhoto = {
+    image: 'cottoncanvas.jpg',
+    description: 'A Cotton Canvas'
+};
 
 let currentPhoto = 0;
 
+let slides = [mountainsPhoto, peoplePhoto, girlPhoto, cottonPhoto];
 
 function changeNextPhoto() {
     currentPhoto = currentPhoto + 1;
 
-    if(currentPhoto === img.length) {
+    if(currentPhoto === slides.length) {
         currentPhoto = 0;
     }
-
-    document.getElementById("home-background").style.backgroundImage = `url(${img[currentPhoto]})`;
-    
+    changeSlide();
 };
 
 function changePreviousPhoto() {
@@ -22,13 +39,19 @@ function changePreviousPhoto() {
     else {
     currentPhoto = currentPhoto - 1;
     }
-   
-    document.getElementById("home-background").style.backgroundImage = `url(${img[currentPhoto]})`;
+   changeSlide();
 };
 
-document.getElementById("next-photo").addEventListener('click', function() {
+function changeSlide() {
+
+    document.getElementById('home-background').style.backgroundImage = `url(${slides[currentPhoto].image})`;
+    
+    document.getElementById('home-description').innerHTML = slides[currentPhoto].description;
+};
+
+document.getElementById('next-photo').addEventListener('click', function() {
     changeNextPhoto()
 });
-document.getElementById("prev-photo").addEventListener('click', function() {
+document.getElementById('prev-photo').addEventListener('click', function() {
     changePreviousPhoto()
 });
